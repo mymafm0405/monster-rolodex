609da7bf63cf74998328bdc809bb73a532a07414
Mahmoud <mymafm0405@gmail.com>
2020-10-06 12:08:12 +0300

    Updates

---
+++ precache-manifest.579332c05ef7cb60e02d26e040652ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad72c3299a3163303eab670d329a2040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9a4b6af2bfe2836ddb1",
+    "url": "/static/css/main.15f7cecc.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a4b6af2bfe2836ddb1",
+    "url": "/static/js/main.8e28264e.chunk.js"
+  },
+  {
+    "revision": "6ee33a9eb5113d8c4c15",
+    "url": "/static/js/runtime-main.52709719.js"
+  }
+]);
